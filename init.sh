8c7360219901d3792f8584b0187836d4876e9365
xjiaqing <ballieasor@outlook.com>
2022-08-22 03:54:07 +0000

    remove sudo from script

--- init.sh
+++ init.sh
@@ -2,13 +2,17 @@
 
 set -eux
 
-sudo useradd -r -M -s /sbin/nologin zookeeper
+id zookeeper >& /dev/null
+if [ $? -ne 0 ]
+then
+    useradd -r -M -s /sbin/nologin zookeeper
+fi
 
-rm -rf /tmp/apache-zookeeper-3.7.1-bin.tar.gz apache-zookeeper-*
-wget -P /tmp https://dlcdn.apache.org/zookeeper/zookeeper-3.7.1/apache-zookeeper-3.7.1-bin.tar.gz
+rm -rf /tmp/apache-zookeeper-3.7.1-bin.tar.gz apache-zookeeper-* /tmp/zookeeper /usr/local/zookeeper
+wget -q -P /tmp https://dlcdn.apache.org/zookeeper/zookeeper-3.7.1/apache-zookeeper-3.7.1-bin.tar.gz
 tar zxf /tmp/apache-zookeeper-3.7.1-bin.tar.gz
-sudo mv apache-zookeeper-3.7.1-bin /usr/local/zookeeper
-sudo chown -R zookeeper:zookeeper /usr/local/zookeeper
+mv apache-zookeeper-3.7.1-bin /usr/local/zookeeper
+chown -R zookeeper:zookeeper /usr/local/zookeeper
 
 mkdir -p /data/zookeeper/{data,logs}
-sudo chown -R zookeeper:zookeeper /data/zookeeper
+chown -R zookeeper:zookeeper /data/zookeeper
